b3a3227a43b28676ad35572cde4e31ed27d6a7fa
Akho Tshweza <atshweza@gmail.com>
2025-08-04 20:20:59 +0200

    Add Current API

---
+++ app/api/weather/current/route.ts
@@ -0,0 +1,23 @@
+import { publicIpv4 } from 'public-ip';
+
+import { appConfig } from '@/utils/configs';
+
+export async function GET() {
+  const ip = await publicIpv4();
+  try {
+    const options = {
+      method: 'GET',
+    };
+    const url = `${appConfig.apiWeatherBaseUrl}/current?access_key=${appConfig.apiKey}&query=${encodeURIComponent(ip)}`;
+    console.log('url', url);
+    const response = await fetch(url, options);
+
+    if (!response.ok) {
+      return new Response('Failed to fetch weather', { status: response.status });
+    }
+    const result = await response.json();
+    return Response.json(result);
+  } catch (error) {
+    return new Response(`Something went wrong fetching Weather`, { status: 500 });
+  }
+}
---
+++ utils/configs.ts
@@ -0,0 +1,4 @@
+export const appConfig = {
+  apiKey: process.env.NEXT_PUBLIC_API_KEY!,
+  apiWeatherBaseUrl: process.env.NEXT_PUBLIC_API_WEATHER_BASE_URL!,
+};
